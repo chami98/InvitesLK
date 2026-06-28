01b8d6fa5518127e2334a0f3a3b10ad6ed57ad2b
chami98 <chamikara98@gmail.com>
2026-06-28 19:07:48 +0530

    Remove body background from globals.css to allow pages to set their own backgrounds

--- app/rsvp/[coupleSlug]/page.tsx
+++ app/rsvp/[coupleSlug]/page.tsx
@@ -78,9 +78,9 @@ export default async function RSVPDashboardPage({ params }: PageProps) {
         coupleSlug={coupleSlug}
         coupleNames={`${couple.partnerA} & ${couple.partnerB}`}
       >
-        <div>
-      {/* Decorative gradient orbs */}
-      <div className="fixed inset-0 overflow-hidden pointer-events-none">
+        <div className="relative w-full">
+          {/* Decorative gradient orbs */}
+          <div className="fixed inset-0 overflow-hidden pointer-events-none">
         <div className="absolute top-0 left-1/4 w-96 h-96 bg-blue-500/10 rounded-full blur-3xl" />
         <div className="absolute bottom-0 right-1/4 w-96 h-96 bg-purple-500/10 rounded-full blur-3xl" />
       </div>
